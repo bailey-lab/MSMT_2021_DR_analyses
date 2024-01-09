637e51384d4ed2d313fa90306aec32b99cca77fc
Alfred Simkin <alfred.simkin@gmail.com>
2024-01-09 16:15:42 +0000

    reverting a change until I can fix

--- scripts/make_table_threshold_prevalences.py
+++ scripts/make_table_threshold_prevalences.py
@@ -70,14 +70,14 @@ def format_table(prevalence_dict, category_values, output_header, filtered_files
 	format_line(prevalence_dict, 'overall', filtered_files, output_table)
 
 prevalence_dict={}
-category_type, filter_type=sample_column, 'all'
+category_type, filter_type='site', 'all'
 categories=set([])
 output_header=[category_type]
 filtered_files=[]
 for prevalence_file in prevalence_files:
 	prefix='_'.join(prevalence_file.split('_')[:-1])
 	mutation, cov, alt, count=prevalence_file.split('/')[-1].split('_')[1:5]
-	obs_category, obs_filter=sample_column, 'all'
+	obs_category, obs_filter='site', 'all'
 	if obs_filter==filter_type and obs_category==category_type:
 		filtered_files.append(prevalence_file)
 		output_header.append(mutation)
