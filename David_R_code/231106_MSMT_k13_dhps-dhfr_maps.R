a94ba0ed8fd909dcb3528e9f3d5dd01edeaed02f
Alfred Simkin <alfred.simkin@gmail.com>
2024-03-28 21:08:02 +0000

    fixed fig2b boundary issues

--- David_R_code/231106_MSMT_k13_dhps-dhfr_maps.R
+++ David_R_code/231106_MSMT_k13_dhps-dhfr_maps.R
@@ -76,7 +76,6 @@ fig2a <- ggplot()+
   theme(legend.title=element_text(size=12))
 
 
-
 #save plot
 ggsave("fig2A_k13_R561H.svg", dpi=600, width=6, height=5)
 ggsave("fig2A_k13_R561H.png", dpi=600, width=6, height=5)
@@ -137,7 +136,6 @@ fig2d <- ggplot()+
   theme(legend.title=element_blank())+
   theme(legend.text=element_text(size=12))+
   theme(legend.title=element_text(size=12))
-
 #save plot
 ggsave("fig2d_dhps_581.svg", dpi=600, width=6, height=5)
 ggsave("fig2d_dhps_581.png", dpi=600, width=6, height=5)
@@ -203,8 +201,6 @@ fig2c <- ggplot()+
   theme(legend.title=element_blank())+
   theme(legend.text=element_text(size=12))+
   theme(legend.title=element_text(size=12))
-
-
 #save plot
 ggsave("fig2c_dhfr_I164L.svg", dpi=600, width=6, height=5)
 ggsave("fig2c_dhfr_I164L.png", dpi=600, width=6, height=5)
@@ -231,9 +227,9 @@ fig2b <- ggplot()+
   geom_label_repel(data = kagera_aatable_bin_561_district, aes(x = lon, y = lat, label=name), size = 5,
                    color= "black", 
                    min.segment.length = 0,
-                   nudge_x = -0.2,
+                   nudge_x = -0.1,
                    box.padding = 0.1,
-                   nudge_y = -0.1,
+                   nudge_y = 0.1,
                    segment.curvature = -0.1,
                    segment.ncp = 3,
                    segment.angle = 20) +
@@ -252,11 +248,10 @@ fig2b <- ggplot()+
   annotate("text", x = 30.9, y = -0.9, label = "Uganda",
            color="grey60", size=4 , fontface="italic") +
   coord_sf(xlim = c(30.3, 32), ylim = c(-0.89,-3.4), expand = TRUE) +
-  #theme(plot.background = element_rect(fill = "white"))+
+  theme(plot.background = element_rect(fill = "white"))+
   theme(legend.text=element_text(size=12))+
   theme(legend.title=element_text(size=12)) +
   theme_void()
-
 
 #save plot
 ggsave("fig2B_kagera_R561H.svg", dpi=600, width=5, height=6)
@@ -264,18 +259,8 @@ ggsave("fig2B_kagera_R561H.png", dpi=600, width=5, height=6)
 
 library(patchwork)
 
-design <- "
-AAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBB#
-AAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBB#
-CCCCCCCCCCCCCCCCCCCDDDDDDDDDDDDDDDDDDD
-CCCCCCCCCCCCCCCCCCCDDDDDDDDDDDDDDDDDDD
-"
-
 fig2a + fig2b + fig2c + fig2d +
   plot_layout(ncol = 2, nrow = 2) +
   plot_annotation(tag_levels = "A")
 
-fig2a + fig2b + fig2c + fig2d +
-  plot_layout(design=design) +
-  plot_annotation(tag_levels = "A")
-ggsave("Fig2.svg", dpi = 600, width = 15, height = 13)
+ggsave("Fig2.svg", dpi = 600, width = 16, height = 16)
